10cf84dd26196f3ac33a022944a2027bd7ed9955
Dora-tech <dorisfrontend@gmail.com>
2024-02-22 13:41:49 -0500

    corrigiendo

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,32 @@ function toggleMenu() {
     toggle_close.style.display = 'none';
   }
 }
+
+const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const id = entry.target.getAttribute("id");
+      const menuLink = document.querySelector(`.menu a[href="#${id}"]`);
+
+      if (entry.isIntersecting) {
+        document.querySelector(".menu a.selected").classList.remove("selected");
+        menuLink.classList.add("selected");
+      }
+    });
+  },
+  { rootMargin: "-30% 0px -70% 0px" }
+);
+
+menuLinks.forEach((menuLink) => {
+  menuLink.addEventListener("click", function () {
+    menu.classList.remove('show-menu');
+  });
+
+  const hash = menuLink.getAttribute("href");
+  const target = document.querySelector(hash);
+  if (target) {
+    observer.observe(target);
+  }
+});
